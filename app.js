e67c143f0ae6de84f0b9032b34c5c6fd3e6f3de4
Bcisternas1 <bj.cisternas@gmail.com>
2025-03-17 16:12:49 -0300

    Sortear amigo
    Se realiza la función en que se leerá la lista de nombres previamente ingresados para darnos un ganador

--- app.js
+++ app.js
@@ -22,6 +22,18 @@ function renderizarAmigos(){
     for(let i = 0; i < amigo.length; i++){
         let item = document.createElement("li");
         item.textContent = amigo[i];
-        listaAmigos.appendChild(item)
-;    }
+        listaAmigos.appendChild(item);}
+}
+
+function sortearAmigo(){
+    if(amigo.length === 0){
+        alert ("No hay amigos para sortear");
+        return;
+    }
+        let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `El amigo ganador es ${amigoSorteado}`;
+
+        let limpiarLista = document.getElementById("listaAmigos");
+        limpiarLista.innerHTML = "";
 }
