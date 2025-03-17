b9b73fe3611c21bbc9e9226c92c3ba263ab22584
Bcisternas1 <bj.cisternas@gmail.com>
2025-03-17 16:07:16 -0300

    Añadir amigos
    Se crea la función de añadir amigos a una lista, y que se de un mensaje de alerta en caso de no tener escrito algo en el campo de texto.

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombresSorteo = []
+et amigo = [];
+
+function agregarAmigo (){
+    let inputAmigo = document.getElementById("amigo") //Para obtener lo digitado en el campo de texto
+    let nombreAmigo = inputAmigo.value; // añadir amigo a la lista
+
+    if(!nombreAmigo){
+        alert("Debes ingresar un nombre");// mensaje en caso de no ingresar valor en campo de texto
+        return;
+    }
+    amigo.push(nombreAmigo); //añadir a lista de sorteo
+    inputAmigo.value = ""; // Limpiar campo de texto
+    inputAmigo.focus(); //limpia campo de texto luego de añadir amigo a la lista
+    renderizarAmigos();
+
+}
